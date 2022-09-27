6461ec5013cfe79805338d3d7c31250d650f4688
ADereglazov <bt745412>
2022-09-27 12:19:42 +0300

    create SearchInput component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <DocumentsTools />
+  <DocumentsTools class="app-documents-tools" />
+  <SearchInput class="app-search-input" />
 </template>
 
 <script setup>
 import DocumentsTools from "@/components/DocumentsTools.vue";
+import SearchInput from "@/components/SearchInput.vue";
 </script>
+
+<style scoped>
+.app-documents-tools {
+  margin-bottom: 30px;
+}
+
+.app-search-input {
+  margin-bottom: 35px;
+}
+</style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,11 +1,110 @@
 <template>
-  <input type="text" placeholder="Поиск" />
+  <label class="search-input">
+    <input
+      ref="inputField"
+      v-model="searchString"
+      class="search-input__input"
+      type="text"
+      placeholder="Поиск"
+    />
+    <button
+      v-show="searchString.length > 0"
+      class="search-input__button"
+      type="button"
+      @click="onClickClear"
+    >
+      <ClearIcon />
+    </button>
+  </label>
 </template>
 
 <script>
+import ClearIcon from "@/assets/icons/clear.svg";
+import { ref } from "vue";
+
 export default {
   name: "SearchInput",
+  components: { ClearIcon },
+  setup() {
+    const inputField = ref(null);
+    const searchString = ref("");
+
+    function onClickClear() {
+      searchString.value = "";
+      inputField.value.focus();
+    }
+
+    return {
+      inputField,
+      searchString,
+      onClickClear,
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.search-input {
+  position: relative;
+  display: inline-block;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    display: block;
+    width: 14px;
+    height: 14px;
+    background: url("@/assets/icons/search.svg") no-repeat center center;
+    background-size: contain;
+  }
+
+  &__input {
+    width: 564px;
+    height: 30px;
+    padding-right: 15px;
+    padding-left: 25px;
+    border-top: none;
+    border-right: none;
+    border-bottom: 1px solid #bfc9e0;
+    border-left: none;
+    font-family: inherit;
+    font-size: 15px;
+    font-weight: 400;
+    line-height: normal;
+    color: #000000;
+    outline: none;
+
+    &:focus {
+      border-bottom-color: #0066ff;
+    }
+
+    &::placeholder {
+      font-size: 15px;
+      line-height: normal;
+      color: #8e9cbb;
+    }
+  }
+
+  &__button {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+    padding: 0;
+    border: none;
+    background: none;
+    cursor: pointer;
+
+    &:active {
+      opacity: 0.5;
+    }
+
+    svg {
+      display: block;
+    }
+  }
+}
+</style>
