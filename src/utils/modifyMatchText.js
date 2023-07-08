f581f72410c628994a9fb6f3cedb09cd5bb2c18c
ADereglazov <sanya95@yandex.ru>
2023-07-08 15:32:08 +0300

    refactoring modifyMatchText.js

--- src/utils/modifyMatchText.js
+++ src/utils/modifyMatchText.js
@@ -1,11 +1,13 @@
 export default function modifyMatchText(text, subString) {
-  const match = text.toLowerCase().indexOf(subString.toLowerCase());
+  const regex = new RegExp(subString, "gi");
 
-  if (~match) {
-    const textSubstr = text.substring(match, subString.length + match);
+  if (text.match(regex)) {
     return text.replace(
-      textSubstr,
-      "<span style='color: #ff238d'>" + textSubstr + "</span>"
+      regex,
+      (str) =>
+        "<span style='color: #ff1744; background-color: #ffff00'>" +
+        str +
+        "</span>"
     );
   }
 
